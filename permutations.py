592862b8dd7a2b4498134e3f7eebaa690adfc380
danke <chen918@vip.qq.com>
2019-05-26 11:07:28 +0100

    complete version, left to further understand

---
+++ permutations.py
@@ -0,0 +1,23 @@
+# https://leetcode-cn.com/problems/permutations
+# Try to swap two elements for all the cases. Use backtrack algorithm
+# Hard to understand.
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first = 0):
+            # if all integers are used up
+            if first == n:  
+                output.append(nums[:])
+            for i in range(first, n):
+                # place i-th integer first 
+                # in the current permutation
+                nums[first], nums[i] = nums[i], nums[first]
+                # use next integers to complete the permutations
+                backtrack(first + 1)
+                # backtrack
+                nums[first], nums[i] = nums[i], nums[first]
+        
+        n = len(nums)
+        output = []
+        backtrack()
+        return output
