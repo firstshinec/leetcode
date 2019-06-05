4d9a7e9d7c37b3cba22951e34a8a3a04dcdfa92b
danke <chen918@vip.qq.com>
2019-06-05 23:28:55 +0100

    first version

---
+++ maximumDepthOfBinaryTree.py
@@ -0,0 +1,19 @@
+# https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+# adopt recursion to find the deepest leaf node -> Hannode tower
+# Divide the total task into two continuous tasks
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None: 
+            return 0 
+        else: 
+            left_height = self.maxDepth(root.left) 
+            right_height = self.maxDepth(root.right) 
+            return max(left_height, right_height) + 1 
